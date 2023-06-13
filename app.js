beac8401163b1a3d7eae39031d0ea3a7e862d191
Hema <hemavathi.gadi@gmail.com>
2023-06-13 13:11:43 +0000

    players list after running all APIS

--- app.js
+++ app.js
@@ -40,7 +40,6 @@ app.get("/players/", async (request, response) => {
 // Add player API
 app.post("/players/", async (request, response) => {
   const bodyDetails = request.body;
-
   const { playerName, jerseyNumber, role } = bodyDetails;
   const addPlayerQuery = `
     INSERT INTO
@@ -52,5 +51,48 @@ app.post("/players/", async (request, response) => {
     );`;
   const dbResponse = await db.run(addPlayerQuery);
   const playerId = dbResponse.lastID;
-  response.send({ playerId: playerId });
+  response.send("Player Added to Team");
 });
+
+//Get player API
+app.get("/players/:playerId", async (request, response) => {
+  const { playerId } = request.params;
+  const getPlayerQuery = `
+    SELECT *
+    FROM cricket_team
+    WHERE player_id = ${playerId};
+    `;
+  const player = await db.get(getPlayerQuery);
+  response.send(player);
+});
+
+//Update player API
+app.put("/players/:playerId", async (request, response) => {
+  const { playerId } = request.params;
+  const bodyDetails = request.body;
+  const { playerName, jerseyNumber, role } = bodyDetails;
+  const updatePlayerQuery = `
+    UPDATE 
+        cricket_team
+    SET
+        player_name = '${playerName}',
+        jersey_number = ${jerseyNumber},
+        role = '${role}'
+    WHERE 
+        player_id = ${playerId}
+    ;`;
+  await db.run(updatePlayerQuery);
+  response.send("Player Details Updated");
+});
+
+//Delete player API
+app.delete("/players/:playerId", async (request, response) => {
+  const { playerId } = request.params;
+  const deletePlayerQuery = `
+    DELETE FROM
+        cricket_team
+    WHERE player_id = ${playerId};`;
+  await db.run(deletePlayerQuery);
+  response.send("Player Removed");
+});
+module.exports = app;
